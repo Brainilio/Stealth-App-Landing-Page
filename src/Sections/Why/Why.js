64c821a5fab4c879443c08940272d4b32fa54b9f
B.A. Imambaks Rodrigues <0924777@hr.nl>
2021-01-15 14:42:51 +0100

    fixed why section; nvm im back on htis site

--- src/Sections/Why/Why.js
+++ src/Sections/Why/Why.js
@@ -1,10 +1,7 @@
 import React from "react"
 import "./Why.scss"
-import { ReactComponent as Globe } from "../../Media/Svgs/globe.svg"
 import { ReactComponent as Jumping } from "../../Media/Svgs/jumping-lady.svg"
-import { ReactComponent as Heart } from "../../Media/Svgs/heart.svg"
 import { ReactComponent as Mountains } from "../../Media/Svgs/mountains.svg"
-import { ReactComponent as Plants } from "../../Media/Svgs/plant.svg"
 import { ReactComponent as Hand } from "../../Media/Svgs/hand.svg"
 import { ReactComponent as Blob } from "../../Media/Svgs/blob-left-1.svg"
 import Title from "../../UI/Title/Title"
@@ -15,7 +12,6 @@ const Why = () => {
 			<Title>Why?</Title>
 			<div className="why-reason">
 				<div>
-					<Globe className="globe" />
 					<Jumping className="jumping" />
 				</div>
 				<span>
@@ -25,7 +21,6 @@ const Why = () => {
 			</div>
 			<div className="why-reason">
 				<div>
-					<Heart className="heart" />
 					<Mountains className="mountains" />
 				</div>
 				<span>
@@ -36,7 +31,6 @@ const Why = () => {
 			<div className="why-reason">
 				<div>
 					<Hand className="hand" />
-					<Plants className="plants" />
 				</div>
 				<span>
 					Reason #1 for using this application and supporting motive to this
